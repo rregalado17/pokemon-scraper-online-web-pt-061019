f443d9059dadeb8bf821038a7ce883156178111e
Robinson Regalado <rregalado17@gmail.com>
2019-09-10 20:34:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+  
+  attr_accessor :name, :type, :id, :db 
+  
+
+  
+  def initialize(pokemon)
+    @name = pokemon[name]
+    @type = pokemon[type]
+    @id = pokemon[id]
+    @db = pokemon[db]
+
+  end
+
+  
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+  
+  def self.find(id, db)
+    found = db.execute("SELECT name FROM pokemon WHERE id = ?", id).flatten[0]
+    Pokemon.new(id: found[0], name: found[1], type: found[2], db: db)
+
+  end
+  
 end
